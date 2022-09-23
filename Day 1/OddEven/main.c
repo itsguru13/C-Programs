15c6de48438a80cff6c2cbec90bc71b3000e9be5
Gurucharan Shivakumar <gurushivkrish@gmail.com>
2022-09-23 14:14:20 +0530

    added Day 1 Programs

---
+++ Day 1/OddEven/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  {
+    int x;
+
+    printf("Enter a number: ");
+    scanf("%d", &x);
+
+    if(x % 2 == 0)  {
+        printf("Number %d is even", x);
+    } else {
+        printf("Number %d is odd", x);
+    }
+
+    return 0;
+}
