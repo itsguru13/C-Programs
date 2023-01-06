b90296bcc994d70ab37248cd897fffedb878ef9e
Gurucharan Shivakumar <gurushivkrish@gmail.com>
2023-01-06 18:03:03 +0530

    updated main.c

--- Day 1/Factorial/main.c
+++ Day 1/Factorial/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()  {
     int i, n, result = 1;
 
     printf("Enter a number: ");
-    scanf("%d", &n);
+    if(scanf("%d", &n)) {
+    } else {
+        printf("Enter a number!");
+        exit(-1);
+    }
 
     for(i=1; i<=n; i++)  {
         result = result * i;  
